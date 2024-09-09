d1ebe504237c40aad9c77fffa3feb93867441066
Berrlin <94164744+Berrlin@users.noreply.github.com>
2024-09-09 15:17:28 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cartRoute from "./routes/CartRoute.js";
 import orderRoute from "./routes/OrderRoute.js";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT||4000;
 
 app.use(express.json())
 app.use(cors())
