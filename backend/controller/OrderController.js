62964744f5b262c2cdd078561937e122a4547604
Berrlin <94164744+Berrlin@users.noreply.github.com>
2024-09-09 15:30:45 +0700

    Update OrderController.js

--- backend/controller/OrderController.js
+++ backend/controller/OrderController.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://e-commerence-frontend.onrender.com/";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
